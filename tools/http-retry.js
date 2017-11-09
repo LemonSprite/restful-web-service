e9ba627d0357ee1e05e69561e8b93f61a3a49bb4
lujianan <ljn6176@163.com>
2017-11-09 11:33:50 +0800

    add http retry fn

---
+++ tools/http-retry.js
@@ -0,0 +1,45 @@
+'use strict'
+
+exports.httpRetry = httpRetry;
+
+
+/**
+ * http 请求重试
+ * 
+ * @param {any} fn 
+ * @param {number} [times=3] 
+ * @param {number} [delay=0] 
+ */
+function httpRetry(fn, times = 3, delay = 0) {
+  return fn().catch(err => {
+    if (needRetry(err)) {
+      if (times > 0) {
+        return Promise.delay(delay).then(() =>
+          httpRetry(fn, times - 1, delay)
+        );
+      } else {
+        err.message += '，已重试3次';
+        return Promise.reject(err);
+      }
+    } else {    
+      return Promise.reject(err);
+    }
+  });
+}
+
+/**
+ * 对系统异常进行判断，决定是否重试
+ * 
+ * @param {any} err 
+ * @returns 
+ */
+function needRetry(err) {
+  const errMsg = err.toString().toLowerCase();
+  const isNeed = errMsg.includes('socket hang up')
+    || errMsg.includes('timeout')
+    || errMsg.includes('econnreset')
+    || errMsg.includes('econnrefused')
+    || errMsg.includes('etimedout');
+  logger.info('needRetry Function: ', isNeed, errMsg);
+  return isNeed;
+}
