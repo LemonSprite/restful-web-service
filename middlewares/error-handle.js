7d5e5d2263ed699f3ceb0e9645e70b27fa5810c0
LemonSprite <ljn6176@163.com>
2017-11-12 22:54:12 +0800

    add error handle

--- middlewares/error-handle.js
+++ middlewares/error-handle.js
@@ -1,5 +1,34 @@
 'use strict';
 
-module.exports = {
-  
+/**
+ * 对 controller 进行异常捕捉
+ * @param {any} controllers
+ * @returns {*}
+ */
+module.exports = controllers => {
+  if (typeof controllers === 'function') {
+    return catchError(controllers);
+  }
+  if (typeof controllers === 'object') {
+    Object.keys(controllers)
+      .filter(ctrl => typeof controllers[ctrl] === 'function')
+      .forEach(ctrl => (controllers[ctrl] = catchError(controllers[ctrl])));
+  }
+  return controllers;
+};
+
+/**
+ * 处理错误
+ * @param {function} controller
+ * @returns {function}
+ */
+function catchError(controller) {
+  return (req, res, next) => {
+    const ret = controller.apply(null, arguments);
+    if (ret && typeof ret.then === 'function') {
+      return ret.catch(err => next({code: 500, msg: err.message || err, err: err}));
+    } else {
+      return ret;
+    }
+  };
 }
